8e4ce92b7c7fd1757c1ee087c19474808ab356fc
Fabián Sojo <fabiansojowork@gmail.com>
2022-07-10 21:08:28 -0600

    Commit inicial, con las plantillas hechas

---
+++ src/code/Estudiantes.java
@@ -0,0 +1,85 @@
+
+package code;
+
+/**
+ *
+ * @author Sojo
+ */
+public class Estudiantes {
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private int edad;
+    private String cedula;
+    private String telefono;
+    private String cursos[];
+
+    public Estudiantes(String nombre, String primerApellido, String segundoApellido, int edad, String cedula, String telefono, String[] cursos) {
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.edad = edad;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.cursos = cursos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String[] getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(String[] cursos) {
+        this.cursos = cursos;
+    }
+    
+    
+    
+}
