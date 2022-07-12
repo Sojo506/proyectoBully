981973f95b8e604358d861a199fd50dde443e28b
Fabián Sojo <fabiansojowork@gmail.com>
2022-07-11 18:18:38 -0600

    Se trabaja con panelRegistroEstudiantes: se agregan los textfield, se verifican, etc. Se cambia el size de los paneles, y poco mas

--- src/code/Estudiante.java
+++ src/code/Estudiante.java
@@ -7,23 +7,21 @@ package code;
  */
 
 //prueba
-public class Estudiantes {
+public class Estudiante {
     private String nombre;
     private String primerApellido;
     private String segundoApellido;
-    private int edad;
+    private String edad;
     private String cedula;
     private String telefono;
-    private String cursos[];
 
-    public Estudiantes(String nombre, String primerApellido, String segundoApellido, int edad, String cedula, String telefono, String[] cursos) {
+    public Estudiante(String nombre, String primerApellido, String segundoApellido, String edad, String cedula, String telefono) {
         this.nombre = nombre;
         this.primerApellido = primerApellido;
         this.segundoApellido = segundoApellido;
         this.edad = edad;
         this.cedula = cedula;
         this.telefono = telefono;
-        this.cursos = cursos;
     }
 
     public String getNombre() {
@@ -50,11 +48,11 @@ public class Estudiantes {
         this.segundoApellido = segundoApellido;
     }
 
-    public int getEdad() {
+    public String getEdad() {
         return edad;
     }
 
-    public void setEdad(int edad) {
+    public void setEdad(String edad) {
         this.edad = edad;
     }
 
@@ -72,16 +70,6 @@ public class Estudiantes {
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
-    }
-
-    public String[] getCursos() {
-        return cursos;
-    }
-
-    public void setCursos(String[] cursos) {
-        this.cursos = cursos;
-    }
-    
-    
+    } 
     
 }
